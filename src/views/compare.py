6c117820cc35ab8139305ebe843609693add1352
Daniel Niecke <daniel@niecke-it.de>
2023-01-29 09:13:06 +0100

    add database priv comparison

--- src/views/compare.py
+++ src/views/compare.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint, render_template, request
 
-from db import db
 from model import Server
+from static import comparable_user_attributes, comparable_db_attributes
 
 
 compare = Blueprint("compare", __name__, url_prefix="/compare")
@@ -14,33 +14,17 @@ def servers():
     server1_id = request.form.get("servers1", 0, type=int)
     server2_id = request.form.get("servers2", 0, type=int)
     user_compare = []
+    database_compare = []
 
     if request.method == "POST":
         server1 = Server.query.get_or_404(server1_id)
         server2 = Server.query.get_or_404(server2_id)
 
-        # compare the users
-        users1 = server1.users
-        users2 = server2.users
+        user_compare = compare_users(server1.users, server2.users)
 
-        users2_matched_ids = []
-
-        # a user from users1 must be compared to each user from users2
-        # since they are nerver perfect sorted
-        for u1 in users1:
-            found_u1_matching = False
-            for u2 in users2:
-                found_u1_matching, result = compare_users(u1, u2)
-                if found_u1_matching:
-                    user_compare.append({"user1": u1, "user2": u2, "diffs": result})
-                    users2_matched_ids.append(u2.id)
-                    break
-            if not found_u1_matching:
-                user_compare.append({"user1": u1, "user2": None, "diffs": []})
-
-        for u2 in users2:
-            if u2.id not in users2_matched_ids:
-                user_compare.append({"user1": None, "user2": u2, "diffs": []})
+        database_compare = compare_databases(
+            server1.database_privileges, server2.database_privileges
+        )
 
     return render_template(
         "compare/servers.html",
@@ -48,75 +32,82 @@ def servers():
         server1_id=server1_id,
         server2_id=server2_id,
         user_compare=user_compare,
+        database_compare=database_compare,
     )
 
 
-comparable_user_attributes = [
-    "plugin",
-    "authentication_string",
-    "password_expired",
-    "password_lifetime",
-    "account_locked",
-    "Create_role_priv",
-    "Drop_role_priv",
-    "Password_reuse_history",
-    "Password_reuse_time",
-    "Password_require_current",
-    "User_attributes",
-    "ssl_type",
-    "ssl_cipher",
-    "x509_issuer",
-    "x509_subject",
-    "Select_priv",
-    "Insert_priv",
-    "Update_priv",
-    "Delete_priv",
-    "Create_priv",
-    "Drop_priv",
-    "Reload_priv",
-    "Shutdown_priv",
-    "Process_priv",
-    "File_priv",
-    "Grant_priv",
-    "References_priv",
-    "Index_priv",
-    "Alter_priv",
-    "Show_db_priv",
-    "Super_priv",
-    "Create_tmp_table_priv",
-    "Lock_tables_priv",
-    "Execute_priv",
-    "Repl_slave_priv",
-    "Repl_client_priv",
-    "Create_view_priv",
-    "Show_view_priv",
-    "Create_routine_priv",
-    "Alter_routine_priv",
-    "Create_user_priv",
-    "Event_priv",
-    "Trigger_priv",
-    "Create_tablespace_priv",
-    "max_questions",
-    "max_updates",
-    "max_connections",
-    "max_user_connections",
-]
+def compare_users(users1, users2):
+    user_compare = []
+    users2_matched_ids = []
+
+    # a user from users1 must be compared to each user from users2
+    # since they are nerver perfect sorted
+    for u1 in users1:
+        found_u1_matching = False
+        for u2 in users2:
+            found_u1_matching, result = compare_attributes(
+                u1,
+                u2,
+                comparable_user_attributes.get("first_compare"),
+                comparable_user_attributes.get("second_compare"),
+            )
+            if found_u1_matching:
+                user_compare.append({"user1": u1, "user2": u2, "diffs": result})
+                users2_matched_ids.append(u2.id)
+                break
+        if not found_u1_matching:
+            user_compare.append({"user1": u1, "user2": None, "diffs": []})
+
+    for u2 in users2:
+        if u2.id not in users2_matched_ids:
+            user_compare.append({"user1": None, "user2": u2, "diffs": []})
+
+    return user_compare
 
 
-def compare_users(u1, u2):
-    # if username or host unequal the users are not the same
-    if u1.User != u2.User or u1.Host != u2.Host:
-        return False, []
+def compare_databases(databases1, databases2):
+    database_compare = []
+    databases2_matched_ids = []
 
-    # if one of the following attributes differ then the users
-    # are the same but with different config
+    # a user from users1 must be compared to each user from users2
+    # since they are nerver perfect sorted
+    for d1 in databases1:
+        found_d1_matching = False
+        for d2 in databases2:
+            found_d1_matching, result = compare_attributes(
+                d1,
+                d2,
+                comparable_db_attributes.get("first_compare"),
+                comparable_db_attributes.get("second_compare"),
+            )
+            if found_d1_matching:
+                database_compare.append(
+                    {"database1": d1, "database2": d2, "diffs": result}
+                )
+                databases2_matched_ids.append(d2.id)
+                break
+        if not found_d1_matching:
+            database_compare.append({"database1": d1, "database2": None, "diffs": []})
+
+    for d2 in databases2:
+        if d2.id not in databases2_matched_ids:
+            database_compare.append({"database1": None, "database2": d2, "diffs": []})
+
+    return database_compare
+
+
+def compare_attributes(c1, c2, first_compare, second_compare):
+    for attr in first_compare:
+        if c1.__dict__.get(attr) != c2.__dict__.get(attr):
+            return False, []
+
     diffs = []
-    for attr in comparable_user_attributes:
-        if u1.__dict__.get(attr) != u2.__dict__.get(attr):
+    for attr in second_compare:
+        if c1.__dict__.get(attr) != c2.__dict__.get(attr):
             # if comparing different mysql versions, it is possible that some values are NULL
             # because they don't exsist for one server
-            v1 = u1.__dict__.get(attr) if u1.__dict__.get(attr) else "NULL"
-            v2 = u2.__dict__.get(attr) if u2.__dict__.get(attr) else "NULL"
+            v1 = c1.__dict__.get(attr) if c1.__dict__.get(attr) else "NULL"
+            v2 = c2.__dict__.get(attr) if c2.__dict__.get(attr) else "NULL"
             diffs.append(
                 (
                     attr,
